450c728873969f9696686b99b1a2b41681e1dd45
François Lépine <francois.lepine@gmail.com>
2018-11-23 08:44:08 -0500

    Premier ajout.

---
+++ PoolLight.App/PoolLight.App/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PoolLight.App
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Windows.UI.Xaml.Controls.Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PoolLight.App/PoolLight.App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace PoolLight.App.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public SolidColorBrush CouleurBouton { get; set; }
+
+        public bool EstAllume { get; private set; }
+
+        private void Allumer()
+        {
+            CouleurBouton = new SolidColorBrush(Colors.Green);
+            EstAllume = true;
+        }
+
+        private void Eteindre()
+        {
+            CouleurBouton = new SolidColorBrush(Colors.Black);
+            EstAllume = false;
+        }
+
+        public void Basculer()
+        {
+            if (EstAllume)
+            {
+                Eteindre();
+            }
+            else
+            {
+                Allumer();
+            }
+        }
+    }
+}
