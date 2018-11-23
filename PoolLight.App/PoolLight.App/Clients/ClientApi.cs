6f65936d5e59a92eb0d3d5721829d2b29fa1322e
François Lépine <francois.lepine@gmail.com>
2018-11-23 11:20:42 -0500

    Commentaires.

--- PoolLight.App/PoolLight.App/Clients/ClientApi.cs
+++ PoolLight.App/PoolLight.App/Clients/ClientApi.cs
@@ -3,17 +3,30 @@ using System.Threading.Tasks;
 
 namespace PoolLight.App.Clients
 {
+    /// <summary>
+    /// Implémentation de l'API pour le globe.
+    /// </summary>
     public class ClientApi : Interfaces.IClientApi
     {
+        /// <summary>
+        /// Temps d'attente.
+        /// </summary>
         private static double TEMPS_ATTENTE = 3d;
 
+        /// <summary>
+        /// Allumer le globe.
+        /// </summary>
+        /// <returns>Tâche qui peut être attendue.</returns>
         public Task<bool> AllumerAsync() =>
             Task.Delay(TimeSpan.FromSeconds(TEMPS_ATTENTE))
             .ContinueWith((tachePrecedente) => true);
 
+        /// <summary>
+        /// Éteindre le globe.
+        /// </summary>
+        /// <returns>Tâche qui peut être attendue.</returns>
         public Task<bool> EteindreAsync() =>
             Task.Delay(TimeSpan.FromSeconds(TEMPS_ATTENTE))
             .ContinueWith((tachePrecedente) => true);
-
     }
 }
