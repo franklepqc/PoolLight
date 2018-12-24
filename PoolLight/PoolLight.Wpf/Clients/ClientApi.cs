77a47f7b10905a93c909e3bc2b2dd6db8ef966a5
François Lépine <francois.lepine@gmail.com>
2018-12-23 21:07:18 -0500

    Supprimer les liens avec Azure (pour le moment).

--- PoolLight/PoolLight.Wpf/Clients/ClientApi.cs
+++ PoolLight/PoolLight.Wpf/Clients/ClientApi.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.Azure.Devices.Client;
-using System;
+﻿using System;
 using System.Threading.Tasks;
-using System.Text;
-using Newtonsoft.Json;
 
 namespace PoolLight.Wpf.Clients
 {
@@ -11,79 +8,21 @@ namespace PoolLight.Wpf.Clients
     /// </summary>
     public class ClientApi : Interfaces.IClientApi
     {
-        // Champs.
-        const string IOT_HUB_CONN_STRING = "HostName=Piscine.azure-devices.net;DeviceId=Device1;SharedAccessKey=NQrYf3ceEfGeO5qkC6Ds4eH0D0lNiFZkt1TujAMbMhQ=";
-        const string IOT_HUB_DEVICE_LOCATION = "canadaeast";
-        const string IOT_HUB_DEVICE = "Device1";
-
         /// <summary>
         /// Allumer le globe.
         /// </summary>
         /// <returns>Tâche qui peut être attendue.</returns>
-        public async Task<bool> AllumerAsync()
-        {
-            var deviceClient = ObtenirClient();
-            try
-            {
-                var msg = new Message(ConstruireMessage("close"));
-                await deviceClient.SendEventAsync(msg);
-                return true;
-            }
-            catch
-            {
-            }
-
-            return false;
-        }
+        public Task<bool> AllumerAsync() =>
+            Task.Delay(TimeSpan.FromSeconds(3d))
+                .ContinueWith(tachePrecedente => true);
 
         /// <summary>
         /// Éteindre le globe.
         /// </summary>
         /// <returns>Tâche qui peut être attendue.</returns>
-        public async Task<bool> EteindreAsync()
-        {
-            var deviceClient = ObtenirClient();
-            try
-            {
-                var msg = new Message(ConstruireMessage("open"));
-                await deviceClient.SendEventAsync(msg);
-                return true;
-            }
-            catch
-            {
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// Construire le message.
-        /// </summary>
-        /// <param name="etat">État du statut.</param>
-        /// <returns></returns>
-        private byte[] ConstruireMessage(string etat) =>
-            Encoding.UTF8.GetBytes(ConstruireContenuMessage(etat));
-
-        /// <summary>
-        /// Construire le contenu pour le message.
-        /// </summary>
-        /// <param name="etat"></param>
-        /// <returns></returns>
-        private string ConstruireContenuMessage(string etat) =>
-            JsonConvert.SerializeObject(new
-            {
-                deviceId = IOT_HUB_DEVICE,
-                location = IOT_HUB_DEVICE_LOCATION,
-                state = etat,
-                localTimestamp = DateTime.Now.ToLocalTime()
-            });
-
-        /// <summary>
-        /// Obtenir le client.
-        /// </summary>
-        /// <returns>Un objet de type DeviceClient.</returns>
-        private DeviceClient ObtenirClient() => 
-            DeviceClient.CreateFromConnectionString(IOT_HUB_CONN_STRING, TransportType.Amqp);
+        public Task<bool> EteindreAsync() =>
+            Task.Delay(TimeSpan.FromSeconds(3d))
+                .ContinueWith(tachePrecedente => true);
 
         /// <summary>
         /// Obtenir la température.
