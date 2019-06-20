53ca00da3670a0da01c9426689b653d0de6cba96
François Lépine <francois.lepine@gmail.com>
2019-06-20 15:19:35 -0400

    Utilisation d'un feature C# 8.0

--- PoolLight/PoolLight.Wpf/Services/ConvertirTemperature.cs
+++ PoolLight/PoolLight.Wpf/Services/ConvertirTemperature.cs
@@ -3,21 +3,23 @@ using System;
 
 namespace PoolLight.Wpf.Services
 {
+    /// <summary>
+    /// Classe qui permet de faire la conversion de la temperature.
+    /// </summary>
     public class ConvertirTemperature : Interfaces.IConvertirTemperature
     {
-        public float Convertir(ModeTempEnum mode, float temperatureEnCelcius)
-        {
-            switch (mode)
+        /// <summary>
+        /// Convertir la température (en celcius), en unité voulue.
+        /// </summary>
+        /// <param name="mode">Unité voulue.</param>
+        /// <param name="temperatureEnCelcius">Température en Celcius.</param>
+        /// <returns>Température à la nouvelle unité.</returns>
+        public float Convertir(ModeTempEnum mode, float temperatureEnCelcius) =>
+            mode switch
             {
-                case ModeTempEnum.Celcius:
-                    return temperatureEnCelcius;
-
-                case ModeTempEnum.Fahrenheit:
-                    return (float)Math.Round((temperatureEnCelcius * 1.8f) + 32f, 0, MidpointRounding.AwayFromZero);
-
-                default:
-                    throw new NotImplementedException();
-            }
-        }
+                ModeTempEnum.Celcius => temperatureEnCelcius,
+                ModeTempEnum.Fahrenheit => (float)Math.Round((temperatureEnCelcius * 1.8f) + 32f, 0, MidpointRounding.AwayFromZero),
+                _ => throw new NotImplementedException()
+            };
     }
 }
